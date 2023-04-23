0f77b5ed09ce9a6775b1d6960c1261e4a8a967aa
Sergey Kuzhel <serik31000@gmail.com>
2023-04-23 17:54:42 +0300

    Web3

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$dsn = "mysql:host=localhost;dbname=lab3var1";
+$user = 'root';
+$pass = '';
+
+try {
+    $dbh = new PDO($dsn, $user, $pass);
+} catch (PDOException $ex) {
+    echo $ex->GetMessage();
+}
+?>
